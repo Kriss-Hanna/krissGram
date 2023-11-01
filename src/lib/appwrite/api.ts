555da34e3dd535f47cfce93870572800219d897f
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-11-01 17:15:16 +0100

    add context & AuthContext component

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-import { ID } from "appwrite";
+import { ID, Query } from "appwrite";
 import {
   account,
   appwriteConfig,
@@ -61,6 +61,29 @@ export async function saveUserToDB(user: {
     );
 
     return newUser;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [
+        Query.equal("accountId", currentAccount.$id),
+        Query.equal("email", currentAccount.email),
+      ]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
   } catch (err) {
     console.log(err);
   }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import AuthProvider from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
