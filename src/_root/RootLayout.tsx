4561260520f1e820022f749eeabb43a053c066fa
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-11-05 15:42:39 +0100

    RootLayout components & HomePage

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import TopBar from "@/components/shared/TopBar";
+import BottomBar from "@/components/shared/BottomBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <div className="w-full md:flex">
+      <TopBar />
+      <LeftSidebar />
 
-export default RootLayout
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
+  );
+};
+
+export default RootLayout;
